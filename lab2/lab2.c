43a40196d823715c6ca0df54cecb29b684f1423e
Maxim <62403261+maxwolf8852@users.noreply.github.com>
2020-11-03 01:00:20 +0300

    Modification 2
    Была добавлена проверка на то, что результат получился на 1 разряд больше, чем все слагаемые, а также улучшены некоторые функции.

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -8,26 +8,30 @@ struct vector {
 	char letter;
 	int number;
 	bool is_first;
+	int is_first_end;
 	struct vector* next;
 };
 
-struct vector* setL(struct vector* arr, char l, bool is_first) {
+struct vector* setL(struct vector* arr, char l, bool is_first, int is_first_end) {
 	if (arr == 0) {
 		arr = (struct vector*)malloc(sizeof(struct vector));
 		arr->letter = l, arr->number = -1;
-		arr->next = 0; arr->is_first = is_first;
+		arr->next = 0; arr->is_first = is_first; arr->is_first_end = is_first_end;
 	}
 	else if (arr->letter > l) {
 		struct vector* tmp = arr;
 		arr = (struct vector*)malloc(sizeof(struct vector));
 		arr->letter = l, arr->number = -1;
-		arr->next = tmp; arr->is_first = is_first;
+		arr->next = tmp; arr->is_first = is_first; arr->is_first_end = is_first_end;
 	}
-	else if (arr->letter < l) arr->next = setL(arr->next, l, is_first);
+	else if (arr->letter < l) arr->next = setL(arr->next, l, is_first, is_first_end);
+	else if (is_first_end != 0)
+		arr->is_first_end = is_first_end;
 	return arr;
 }
 
 struct vector* arr = 0;
+bool evr1 = true;
 
 inline int first_bit(int source) {
 	unsigned int i = 0;
@@ -38,17 +42,16 @@ inline int first_bit(int source) {
 
 inline int let_num(char letter) {
 	struct vector* temp = arr;
-	for (int i = 0; temp != 0; i++, temp = temp->next)
+	for (; temp != 0; temp = temp->next)
 		if (temp->letter == letter) return temp->number;
 }
 
 int convert(char* string) {
 	unsigned int result = 0;
-	for (int i = 0; i < strlen(string) - 1; i++) {
-		result += let_num(string[i]);
+	for (int i = 0; i < strlen(string); i++) {
 		result *= 10;
+		result += let_num(string[i]);
 	}
-	result += let_num(string[strlen(string) - 1]);
 	return result;
 }
 
@@ -57,11 +60,11 @@ bool check(int argc, char* argv[]) {
 	for (int i = 1; i < argc - 1; i += 2)
 		result += convert(*(argv + i));
 	if (result == convert(*(argv + argc - 1))) {
-		for (int i = 1; i < argc; i += 2) {
+		for (int i = 1; i < argc - 2; i += 2) {
 			printf("%d", convert(*(argv + i)));
-			if (i < argc - 1)
-				printf(" %s ", (*(argv + i + 1)));
+			printf(" %s ", (*(argv + i + 1)));
 		}
+		printf("%d", convert(*(argv + argc - 1)));
 		return true;
 	}
 	return false;
@@ -69,10 +72,9 @@ bool check(int argc, char* argv[]) {
 
 bool do_puzzle(int remain, int argc, char* argv[], struct vector* temp) {
 	int re = remain;
-	int fb = first_bit(remain);
 	if (temp == 0) { return check(argc, argv); }
-	for (int j = fb; j != 11; re = re & ~(1 << j), j = first_bit(re)) {
-		if (temp->is_first && j == 0) continue;
+	for (int j = first_bit(remain); j != 11; re = re & ~(1 << j), j = first_bit(re)) {
+		if ((temp->is_first && j == 0) || (evr1 && temp->is_first_end != 0 && j > temp->is_first_end)) continue;
 		temp->number = j;
 		if (do_puzzle(remain & ~(1 << j), argc, argv, temp->next)) return true;
 	}
@@ -80,16 +82,25 @@ bool do_puzzle(int remain, int argc, char* argv[], struct vector* temp) {
 }
 
 void parse_line(int argc, char* argv[]) {
-	for (int i = 1; i < argc; i += 2)
-		for (int j = 0; j < strlen(*(argv + i)); j++)
-			arr = setL(arr, *(*(argv + i) + j), j == 0);
+	int z = argc / 2 - 2;
+	int len_end = strlen(*(argv + argc - 1));
+	for (int i = 1; i < argc; i += 2) {
+		int len;
+		if (i != argc - 1) {
+			len = strlen(*(argv + i));
+			if (len >= len_end)
+				evr1 = false;
+		}
+		else  len = len_end;
+		for (int j = 0; j < len; j++)
+			arr = setL(arr, *(*(argv + i) + j), j == 0, (i == argc - 1 && j == 0) ? z : 0);
+	}
 }
 
 int main(int argc, char* argv[])
 {
 	unsigned int start_time = clock();
 	if (argc == 1) { printf("Please, use cmd!"); return 0; }
-
 	parse_line(argc, argv);
 	struct vector* temp = arr;
 	if (!do_puzzle(0b1111111111, argc, argv, temp)) printf("Something went wrong!");
