9e055eadc5a34832d5818e0f693f1f871865d0b2
Maxim <62403261+maxwolf8852@users.noreply.github.com>
2020-11-03 00:53:47 +0300

    Modification 1
    Хранение букв в массиве было заменено на линейный список. Также соответственно был переработан сам алгоритм с учетом нового способа хранения букв.

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -4,15 +4,30 @@
 #include <time.h>
 #include <stdbool.h>
 
-char allletters[2][11] = { "\0\0\0\0\0\0\0\0\0\0\0","\0\0\0\0\0\0\0\0\0\0\0" };
-int count = 0;
+struct vector {
+	char letter;
+	int number;
+	bool is_first;
+	struct vector* next;
+};
 
-inline int count_of_bits(int source) {
-	unsigned int i = 0;
-	for (; source; i++)
-		source &= (source - 1);
-	return i;
+struct vector* setL(struct vector* arr, char l, bool is_first) {
+	if (arr == 0) {
+		arr = (struct vector*)malloc(sizeof(struct vector));
+		arr->letter = l, arr->number = -1;
+		arr->next = 0; arr->is_first = is_first;
+	}
+	else if (arr->letter > l) {
+		struct vector* tmp = arr;
+		arr = (struct vector*)malloc(sizeof(struct vector));
+		arr->letter = l, arr->number = -1;
+		arr->next = tmp; arr->is_first = is_first;
+	}
+	else if (arr->letter < l) arr->next = setL(arr->next, l, is_first);
+	return arr;
 }
+
+struct vector* arr = 0;
 
 inline int first_bit(int source) {
 	unsigned int i = 0;
@@ -21,11 +36,10 @@ inline int first_bit(int source) {
 	return 11;
 }
 
-int let_num(char letter) {
-	for (int i = 0; allletters[0][i] != 0; i++) {
-		for (int j = 0; j < 10; j++)
-			if (allletters[0][j] == letter) return allletters[1][j] - '0';
-	}
+inline int let_num(char letter) {
+	struct vector* temp = arr;
+	for (int i = 0; temp != 0; i++, temp = temp->next)
+		if (temp->letter == letter) return temp->number;
 }
 
 int convert(char* string) {
@@ -38,19 +52,10 @@ int convert(char* string) {
 	return result;
 }
 
-inline bool is_firstnull(int argc, char* argv[]) { // начало с 0
-	for (int i = 1; i < argc; i += 2) {
-		if (let_num(*(argv + i)[0]) == 0) return true;  // начало с 0
-	}
-	return false;
-}
-
 bool check(int argc, char* argv[]) {
-	if (is_firstnull(argc, argv)) return false;
 	unsigned int result = 0;
 	for (int i = 1; i < argc - 1; i += 2)
 		result += convert(*(argv + i));
-
 	if (result == convert(*(argv + argc - 1))) {
 		for (int i = 1; i < argc; i += 2) {
 			printf("%d", convert(*(argv + i)));
@@ -62,16 +67,14 @@ bool check(int argc, char* argv[]) {
 	return false;
 }
 
-bool do_puzzle(int remain, int argc, char* argv[]) {
+bool do_puzzle(int remain, int argc, char* argv[], struct vector* temp) {
 	int re = remain;
-	int cntb = count_of_bits(remain);
 	int fb = first_bit(remain);
-	if ((10 - cntb) == count) { return check(argc, argv); }
-	for (int j = fb; j != 11; j = first_bit(re)) {
-		re = re & ~(1 << j);
-		allletters[1][10 - cntb] = j + '0';
-		if (do_puzzle(remain & ~(1 << j), argc, argv)) return true;
-		allletters[1][10 - cntb] = 0;
+	if (temp == 0) { return check(argc, argv); }
+	for (int j = fb; j != 11; re = re & ~(1 << j), j = first_bit(re)) {
+		if (temp->is_first && j == 0) continue;
+		temp->number = j;
+		if (do_puzzle(remain & ~(1 << j), argc, argv, temp->next)) return true;
 	}
 	return false;
 }
@@ -79,25 +82,18 @@ bool do_puzzle(int remain, int argc, char* argv[]) {
 void parse_line(int argc, char* argv[]) {
 	for (int i = 1; i < argc; i += 2)
 		for (int j = 0; j < strlen(*(argv + i)); j++)
-			for (int k = 0; k < 10; k++)
-			{
-				if (*(*(argv + i) + j) == allletters[0][k]) break;
-				if (allletters[0][k] == 0) {
-					allletters[0][k] = *(*(argv + i) + j);
-					if (++count == 10) return;
-					break;
-				}
-			}
+			arr = setL(arr, *(*(argv + i) + j), j == 0);
 }
 
 int main(int argc, char* argv[])
 {
 	unsigned int start_time = clock();
 	if (argc == 1) { printf("Please, use cmd!"); return 0; }
+
 	parse_line(argc, argv);
-	if (!do_puzzle(0b1111111111, argc, argv)) printf("Something went wrong!");
+	struct vector* temp = arr;
+	if (!do_puzzle(0b1111111111, argc, argv, temp)) printf("Something went wrong!");
 
 	printf("\n%.3f\n", (clock() - start_time) / 1000.0f);
-	getc(stdin);
 	return 0;
 }
